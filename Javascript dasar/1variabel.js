55e3edadb2acdb2220f493f237979f1d7b071a3b
FaisalRam02 <141383332+FaisalRam02@users.noreply.github.com>
2024-02-02 10:48:38 +0700

    Update variabel.html

--- Javascript dasar/1variabel.js
+++ Javascript dasar/1variabel.js
@@ -1,1 +1,16 @@
-let animal;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Animal Variable</title>
+</head>
+<body>
+
+<script>
+  // Deklarasi variabel animal menggunakan let
+  let animal;
+</script>
+
+</body>
+</html>
